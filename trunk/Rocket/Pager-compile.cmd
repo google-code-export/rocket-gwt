021aa8c47f8783aef0c198e9207d97c0156141d8
miroslav.pokorny <miroslav.pokorny@c50fa27a-1f1b-0410-a12a-0d92cb1c21c8>
2007-01-17 07:05:11 +0000

    0.24 17 January 2007 -------------------- Download from http://rocket-gwt.googlecode.com/files/Rocket-0.24.zip
    * Added ant build.xml to take care of everything related to building a new release & also removed *.cmd files.
    * Refactored Cookies and support classes to cookies module.
    * Introduced a testing package which mimics JUNIT TestCase and TestRunner. 
      Unlike the GWT supported GwtTestCase which uses a hidden browser the tests are actually a regular module
      with the browser visible at all times.
    * Made just about all jsni methods private except for those that typically have no arguments which are usually 
      tested within their wrapper method.
    * Renamed accompanying impl subpackages and classes to support. These classes should not be used directly in user code.
    * Moved stacktrace support classes into its own jar file (Rocket-stacktrace.jar).
    * Added two new Map views of the inline style object and the computed(current) style for an element.
    * Refactored BrowserHelper browser specific jsni methods to support classes (rocket.browser.client.support.*)
    * Refactored StyleHelper browser specific jsni methods to support classes (rocket.style.client.support.*)
    * Fixed coordinates of element being dragged when the viewport was scrolled.(DraggablePanel$DragHandle)
    * Added more special cases to StyleHelper.getComputedStyle(), getInlineStyle() and setInlineStyle() enabling easy 
      non browser access of various more properties. Eventually hopefully all properties can be accessed without worrying 
      about browser differences.
    * StylePropertyValue objects are now disconnected. To update the backing style property the appropriate Map must be put again.
    * Renamed CometServerConnectionFailure exception to CometServerConnectionFailureException.
    * Renamed Slider.handleMouseDown() to handleHandleMouseDown()
    * Fixed several widgets (Composite widgets) which were sinking their interested event bits and setting their EventListener outside of onAttach. 
    * Renamed WidgetConstants.BREADCRUMB_* to BREADCRUMB_PANEL_*, classname also renamed from rocket-breadcrumbs- to rocket-breadcrumbPanel
    * Deleted DialogBox which was previously the GWT DialogBox class with a few fixes.
    * Fixed FormHelper.getFormElements() iterator to be destroyable allow breakage of cyclic references.
    
    GWT
    FIX com/google/gwt/emul/java/lang/Character.digit(char,index) was failing when converting A-F and a-f from hex to decimal.
        (http://code.google.com/p/google-web-toolkit/issues/detail?id=302)

--- trunk/Rocket/Pager-compile.cmd
+++
@@ -1,6 +0,0 @@
-@echo compiling Pager demo...
-@call setEnv.cmd
-@java -cp "%~dp0\src;%GWTLIBRARIES%" com.google.gwt.dev.GWTCompiler -out "%~dp0\www" %*  -style DETAILED rocket.widget.test.pager.Pager
-
-@del www\rocket.widget.test.pager.Pager\tree*.gif
-@del www\rocket.widget.test.pager.Pager\history.html
--- trunk/Rocket/Style.cmd
+++
@@ -1,1 +0,0 @@
-@java -cp "%~dp0\src;c:/workspace/Rocket/Java Source;%~dp0\bin;C:/java-libraries/gwt-windows-1.2.22/gwt-user.jar;C:/java-libraries/gwt-windows-1.2.22/gwt-dev-windows.jar" com.google.gwt.dev.GWTShell -style DETAILED -out "%~dp0\www" %* rocket.style.test.Style/Style.html
