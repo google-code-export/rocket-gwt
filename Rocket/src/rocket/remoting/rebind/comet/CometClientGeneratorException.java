615709ee58118501ea099f90f3cf822d45d1338d
miroslav.pokorny <miroslav.pokorny@c50fa27a-1f1b-0410-a12a-0d92cb1c21c8>
2007-09-09 03:26:00 +0000

    * Streamlined development effort required to use comet client. Developers no longer need to create the two dummy GWT RPC service and async interfaces. These are automatically generated   by a new CometClientGenerator.

--- Rocket/src/rocket/remoting/rebind/comet/CometClientGeneratorException.java
+++ Rocket/src/rocket/remoting/rebind/comet/CometClientGeneratorException.java
@@ -5,7 +5,7 @@
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ * http:www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
@@ -13,10 +13,24 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
-package rocket.remoting.test.comet.client;
+package rocket.remoting.rebind.comet;
 
-import com.google.gwt.user.client.rpc.RemoteService;
+import rocket.generator.rebind.GeneratorException;
 
-public interface TestCometService extends RemoteService {
-	TestCometPayload invoke();
+public class CometClientGeneratorException extends GeneratorException {
+
+	CometClientGeneratorException() {
+	}
+
+	CometClientGeneratorException(String message) {
+		super(message);
+	}
+
+	CometClientGeneratorException(Throwable cause) {
+		super(cause);
+	}
+
+	CometClientGeneratorException(String message, Throwable cause) {
+		super(message, cause);
+	}
 }
--- Rocket/src/rocket/remoting/test/comet/client/TestCometServiceAsync.java
+++
@@ -1,8 +0,0 @@
-package rocket.remoting.test.comet.client;
-
-import com.google.gwt.user.client.rpc.AsyncCallback;
-import com.google.gwt.user.client.rpc.RemoteService;
-
-public interface TestCometServiceAsync extends RemoteService {
-	void invoke(AsyncCallback callback);
-}
