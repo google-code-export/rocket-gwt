79cd8302fad2bb76175676bc69008bc60cff899b
miroslav.pokorny <miroslav.pokorny@c50fa27a-1f1b-0410-a12a-0d92cb1c21c8>
2007-10-01 04:40:28 +0000

    * Refactored to use Dom.removeFromParent.

--- trunk/Rocket/src/rocket/remoting/client/CometClient.java
+++ trunk/Rocket/src/rocket/remoting/client/CometClient.java
@@ -77,7 +77,7 @@ public abstract class CometClient {
 	public void stop() {
 		if (this.hasFrame()) {
 			final Element frame = this.getFrame();
-			DOM.removeChild(DOM.getParent(frame), frame);
+			Dom.removeFromParent( frame );
 			this.clearFrame();
 			this.getSupport().stop(this, frame);
 		}
