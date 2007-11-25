1f85c67600dbf0f7c59809a86e7ded5135be3936
miroslav.pokorny <miroslav.pokorny@c50fa27a-1f1b-0410-a12a-0d92cb1c21c8>
2007-11-25 06:42:51 +0000

    * Fixed Type.getSubTypes() to only return immediate sub types and not all sub types.

--- Rocket/src/rocket/generator/test/generator/rebind/SubTypesGenerator.java
+++ Rocket/src/rocket/generator/test/generator/rebind/SubTypesGenerator.java
@@ -26,72 +26,68 @@ import rocket.util.client.PrimitiveHelper;
 
 public class SubTypesGenerator extends TestGenerator {
 
-	protected NewConcreteType assembleNewType(final Type subType0, final String newTypeName) {
+	protected NewConcreteType assembleNewType(final Type subType, final String newTypeName) {
 		final GeneratorContext context = this.getGeneratorContext();
 
-		final Set subTypes0 = subType0.getSubTypes();
-		PrimitiveHelper.checkEquals("", 0, subTypes0.size());
+		final Set subTypesSubTypes = subType.getSubTypes();
+		PrimitiveHelper.checkEquals("" + subTypesSubTypes, 0, subTypesSubTypes.size());
 
-		final NewConcreteType subType1 = context.newConcreteType();
-		subType1.setAbstract(false);
-		subType1.setFinal(false);
-		subType1.setName(newTypeName);
-		subType1.setSuperType(subType0);
+		final NewConcreteType newConcreteType = context.newConcreteType();
+		newConcreteType.setAbstract(false);
+		newConcreteType.setFinal(false);
+		newConcreteType.setName( newTypeName );
+		newConcreteType.setSuperType( context.getObject() );
 
-		final NewNestedType subType2 = subType1.newNestedType();
-		subType2.setAbstract(false);
-		subType2.setFinal(false);
-		subType2.setNestedName("Nested1");
-		subType2.setSuperType(subType1);
-		subType2.setVisibility(Visibility.PUBLIC);
+		final NewNestedType newSubType = newConcreteType.newNestedType();
+		newSubType.setAbstract(false);
+		newSubType.setFinal(false);
+		newSubType.setNestedName("Nested1");
+		newSubType.setStatic( true );
+		newSubType.setSuperType( newConcreteType );
+		newSubType.setVisibility(Visibility.PUBLIC);
 
-		final NewNestedType subType3 = subType1.newNestedType();
-		subType3.setAbstract(false);
-		subType3.setFinal(false);
-		subType3.setNestedName("Nested2");
-		subType3.setSuperType(subType2);
-		subType3.setVisibility(Visibility.PUBLIC);
+		final NewNestedType newSubSubType0 = newSubType.newNestedType();
+		newSubSubType0.setAbstract(false);
+		newSubSubType0.setFinal(false);
+		newSubSubType0.setNestedName("Nested2");
+		newSubSubType0.setStatic( true );
+		newSubSubType0.setSuperType( newSubType );
+		newSubSubType0.setVisibility(Visibility.PUBLIC);
 
-		final NewNestedType subType4 = subType1.newNestedType();
-		subType4.setAbstract(false);
-		subType4.setFinal(false);
-		subType4.setNestedName("Nested3");
-		subType4.setSuperType(subType3);
-		subType4.setVisibility(Visibility.PUBLIC);
-
-		final Set subTypes4 = subType4.getSubTypes();
-		PrimitiveHelper.checkEquals("", 0, subTypes4.size());
-
-		final Set subTypes3 = subType3.getSubTypes();
-		PrimitiveHelper.checkEquals("", 1, subTypes3.size());
-		PrimitiveHelper.checkTrue("SubType4", subTypes3.contains(subType4));
-
-		final Set subTypes2 = subType2.getSubTypes();
-		PrimitiveHelper.checkEquals("", 2, subTypes2.size());
-		PrimitiveHelper.checkTrue("SubType4", subTypes2.contains(subType4));
-		PrimitiveHelper.checkTrue("SubType3", subTypes2.contains(subType3));
-
-		final Set subTypes1 = subType1.getSubTypes();
-		PrimitiveHelper.checkEquals("", 3, subTypes1.size());
-		PrimitiveHelper.checkTrue("SubType4", subTypes1.contains(subType4));
-		PrimitiveHelper.checkTrue("SubType3", subTypes1.contains(subType3));
-		PrimitiveHelper.checkTrue("SubType2", subTypes1.contains(subType2));
-
-		final Set subTypes0b = subType0.getSubTypes();
-		PrimitiveHelper.checkEquals("", 4, subTypes0b.size());
-		PrimitiveHelper.checkTrue("SubType4", subTypes0b.contains(subType4));
-		PrimitiveHelper.checkTrue("SubType3", subTypes0b.contains(subType3));
-		PrimitiveHelper.checkTrue("SubType2", subTypes0b.contains(subType2));
-		PrimitiveHelper.checkTrue("SubType1", subTypes0b.contains(subType1));
-
+		final NewNestedType newSubSubType1 = newSubType.newNestedType();
+		newSubSubType1.setAbstract(false);
+		newSubSubType1.setFinal(false);
+		newSubSubType1.setNestedName("Nested3");
+		newSubSubType1.setStatic( true );
+		newSubSubType1.setSuperType( newSubType );
+		newSubSubType1.setVisibility(Visibility.PUBLIC);
+		
+		final NewNestedType newSubSubSubType = newConcreteType.newNestedType();
+		newSubSubSubType.setAbstract(false);
+		newSubSubSubType.setFinal(false);
+		newSubSubSubType.setNestedName("Nested4");
+		newSubSubSubType.setStatic( true );
+		newSubSubSubType.setSuperType( newSubSubType0 );
+		newSubSubSubType.setVisibility(Visibility.PUBLIC);
+		
 		final Set objectSubTypes = context.getObject().getSubTypes();
-		PrimitiveHelper.checkTrue("SubType4", objectSubTypes.contains(subType4));
-		PrimitiveHelper.checkTrue("SubType3", objectSubTypes.contains(subType3));
-		PrimitiveHelper.checkTrue("SubType2", objectSubTypes.contains(subType2));
-		PrimitiveHelper.checkTrue("SubType1", objectSubTypes.contains(subType1));
-		PrimitiveHelper.checkTrue("SubType0", objectSubTypes.contains(subType0));
+		PrimitiveHelper.checkTrue("newConcreteType" + objectSubTypes, objectSubTypes.contains(newConcreteType));
 		PrimitiveHelper.checkTrue("String", objectSubTypes.contains(context.getString()));
 
-		return subType1;
+		final Set newConcreteSubTypes = newConcreteType.getSubTypes();
+		PrimitiveHelper.checkEquals("", 1, newConcreteSubTypes.size());
+		PrimitiveHelper.checkTrue("Nested1", newConcreteSubTypes.contains(newSubType));
+
+		final Set newSubTypeSubTypes = newSubType.getSubTypes();
+		PrimitiveHelper.checkEquals("", 2, newSubTypeSubTypes.size());
+		PrimitiveHelper.checkTrue("Nested2", newSubTypeSubTypes.contains(newSubSubType0));
+		PrimitiveHelper.checkTrue("Nested3", newSubTypeSubTypes.contains(newSubSubType1));
+
+		final Set newSubSubSubTypeSubTypes = newSubSubType0.getSubTypes();
+		PrimitiveHelper.checkEquals("", 1, newSubSubSubTypeSubTypes.size());
+		PrimitiveHelper.checkTrue("Nested4", newSubSubSubTypeSubTypes.contains(newSubSubSubType));
+		
+		
+		return newConcreteType;
 	}
 }
