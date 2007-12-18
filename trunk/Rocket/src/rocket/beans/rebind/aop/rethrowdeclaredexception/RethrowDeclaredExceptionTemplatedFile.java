be3f10f37d6e3cd40c9a798026de52b167b9043c
miroslav.pokorny <miroslav.pokorny@c50fa27a-1f1b-0410-a12a-0d92cb1c21c8>
2007-12-18 10:51:30 +0000

    * Refactored bean factory generator support classes(mostly moved templated files into their own subpackages). * Improved logging produced by BeanFactoryGenerator into a logically formatted tree structure(was previously very flat).

---
+++ trunk/Rocket/src/rocket/beans/rebind/aop/rethrowdeclaredexception/RethrowDeclaredExceptionTemplatedFile.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright Miroslav Pokorny
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package rocket.beans.rebind.aop.rethrowdeclaredexception;
+
+import java.io.InputStream;
+
+import rocket.generator.rebind.codeblock.TemplatedCodeBlock;
+import rocket.generator.rebind.codeblock.TemplatedCodeBlockException;
+import rocket.generator.rebind.type.Type;
+import rocket.util.client.ObjectHelper;
+
+/**
+ * An abstraction for the rethrow declared exception template
+ * 
+ * @author Miroslav Pokorny
+ */
+public class RethrowDeclaredExceptionTemplatedFile extends TemplatedCodeBlock {
+
+	public RethrowDeclaredExceptionTemplatedFile() {
+		super();
+		setNative(false);
+	}
+
+	/**
+	 * The method exception being unwrapped
+	 */
+	private Type exception;
+
+	protected Type getException() {
+		ObjectHelper.checkNotNull("field:exception", exception);
+		return this.exception;
+	}
+
+	public void setException(final Type exception) {
+		ObjectHelper.checkNotNull("exception:exception", exception);
+		this.exception = exception;
+	}
+
+	protected String getFileName() {
+		return Constants.TEMPLATE;
+	}
+
+	public InputStream getInputStream() {
+		final String filename = this.getFileName();
+		final InputStream inputStream = this.getClass().getResourceAsStream(filename);
+		if (null == inputStream) {
+			throw new TemplatedCodeBlockException("Unable to find template file \"" + filename + "\".");
+		}
+		return inputStream;
+	}
+
+	public Object getValue0(final String name) {
+		Object value = null;
+		while (true) {
+			if (Constants.EXCEPTION_TYPE.equals(name)) {
+				value = this.getException();
+				break;
+			}
+			break;
+		}
+		return value;
+	}
+
+	protected void throwValueNotFoundException(final String name) {
+		throw new TemplatedCodeBlockException("Value for placeholder \"" + name + "\" not found, template file \"" + this.getFileName() + "\".");
+	}
+}
