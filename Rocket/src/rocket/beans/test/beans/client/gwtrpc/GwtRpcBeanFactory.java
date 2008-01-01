f04552fe8d500866f9073d12f76091c543a2940c
miroslav.pokorny <miroslav.pokorny@c50fa27a-1f1b-0410-a12a-0d92cb1c21c8>
2008-01-01 12:06:58 +0000

    * Minor fix to repository following last commit.

--- Rocket/src/rocket/beans/test/beans/client/gwtrpc/GwtRpcBeanFactory.java
+++ Rocket/src/rocket/beans/test/beans/client/gwtrpc/GwtRpcBeanFactory.java
@@ -13,10 +13,10 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
-package rocket.beans.test.beans.client.remoterpcservice;
+package rocket.beans.test.beans.client.gwtrpc;
 
 import rocket.beans.client.BeanFactory;
 
-public interface RemoteRpcServiceBeanFactory extends BeanFactory {
+public interface GwtRpcBeanFactory extends BeanFactory {
 
 }
