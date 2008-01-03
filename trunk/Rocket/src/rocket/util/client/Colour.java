68d96eb109b99f7ffd48dc8738a387dbe4a34d7b
miroslav.pokorny <miroslav.pokorny@c50fa27a-1f1b-0410-a12a-0d92cb1c21c8>
2008-01-03 10:44:49 +0000

    * Added no arguments constructor to make Colour serializable.

--- trunk/Rocket/src/rocket/util/client/Colour.java
+++ trunk/Rocket/src/rocket/util/client/Colour.java
@@ -316,6 +316,10 @@ public class Colour implements java.io.Serializable, IsSerializable {
 	public Colour(final int colour) {
 		this((colour >> 16) & 0xff, (colour >> 8) & 0xff, colour & 0x0ff);
 	}
+	
+	public Colour(){
+		this( 0 );
+	}
 
 	public Colour(final int red, final int green, final int blue) {
 		super();
