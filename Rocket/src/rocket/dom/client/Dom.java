28419e1c668a8a00c3d9407ba0d390353230dd7c
miroslav.pokorny <miroslav.pokorny@c50fa27a-1f1b-0410-a12a-0d92cb1c21c8>
2008-01-05 06:31:55 +0000

    * Renamed WidgetsHelper to Widgets, as well moving various form related helper methods to Dom.

--- Rocket/src/rocket/dom/client/Dom.java
+++ Rocket/src/rocket/dom/client/Dom.java
@@ -16,12 +16,16 @@
 package rocket.dom.client;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import rocket.collection.client.CollectionsHelper;
 import rocket.util.client.Checker;
+import rocket.util.client.Destroyable;
 import rocket.util.client.JavaScript;
 
+import com.google.gwt.http.client.URL;
 import com.google.gwt.user.client.DOM;
 import com.google.gwt.user.client.Element;
 
@@ -346,4 +350,193 @@ public class Dom {
 	public static boolean isAttached(final Element element) {
 		return DOM.isOrHasChild(Dom.getBody(), element);
 	}
+	
+	/**
+	 * Populates the given map with the values of the elements belonging to the
+	 * given form. The element name becomes the key and teh value the entry
+	 * value.
+	 * 
+	 * FormElements without a name are skipped.
+	 * 
+	 * @param map
+	 *            The destination map
+	 * @param form
+	 *            The form containing the elements.
+	 */
+	public static void populateMapFromForm(final Map map, final Element form) {
+		Checker.notNull("parameter:map", map);
+		Checker.notNull("parameter:form", form);
+
+		final Iterator formElements = Dom.getFormElements(form);
+		while (formElements.hasNext()) {
+			final Element formElement = (Element) formElements.next();
+			final String name = DOM.getElementProperty(formElement, DomConstants.NAME);
+			if (null == name) {
+				continue;
+			}
+			final String value = Dom.getFormSubmitValue(formElement);
+
+			map.put(name, value);
+		}
+	}
+
+	/**
+	 * Encodes all the elements belonging to form into a safe url encoded
+	 * String.
+	 * 
+	 * @param form
+	 * @return
+	 */
+	public static String urlEncodeForm(final Element form) {
+		Checker.notNull("parameter:form", form);
+
+		final StringBuffer urlEncoded = new StringBuffer();
+		boolean addSeparator = false;
+
+		final Iterator formElements = Dom.getFormElements(form);
+		while (formElements.hasNext()) {
+			if (addSeparator) {
+				urlEncoded.append('&');
+			}
+
+			final Element formElement = (Element) formElements.next();
+			final String name = JavaScript.getString(JavaScript.castFromElement(formElement), DomConstants.NAME);
+			final String value = URL.encodeComponent(Dom.getFormSubmitValue(formElement));
+			urlEncoded.append(name);
+			urlEncoded.append('=');
+			urlEncoded.append(value);
+
+			addSeparator = true;
+		}
+
+		return urlEncoded.toString();
+	}
+
+	/**
+	 * This method is smart in that it tests the tag type of the given element
+	 * and reads the appropriate attribute that contains the textual value of
+	 * this element. This is the value that would have been submitted for this
+	 * field if its parent form was submitted.
+	 * 
+	 * @param element
+	 * @return
+	 */
+	public static String getFormSubmitValue(final Element element) {
+		Checker.notNull("parameter:element", element);
+
+		String value = null;
+		while (true) {
+			if (Dom.isTag(element, DomConstants.LISTBOX_TAG)) {
+				value = DOM.getElementProperty(element, DomConstants.VALUE);
+				break;
+			}
+			if (Dom.isTag(element, DomConstants.TEXTAREA_TAG)) {
+				value = DOM.getInnerText(element);
+				break;
+			}
+
+			if (Dom.isTag(element, DomConstants.INPUT_TAG)) {
+				value = DOM.getElementProperty(element, DomConstants.VALUE);
+				break;
+			}
+
+			throw new UnsupportedOperationException("Cannot get the formSubmitValue for the element, element: " + DOM.toString(element));
+		}
+
+		return value;
+	}
+
+	/**
+	 * Returns an iterator which may be used to visit all the elements for a
+	 * particular form. Because the form.elements collection cannot have
+	 * elements added / removed this iterator is read only(aka the remove() )
+	 * doesnt work. The iterator is also not fail safe.
+	 * 
+	 * @param form
+	 * @return
+	 */
+	public static Iterator getFormElements(final Element form) {
+		Dom.checkTagName("parameter:form", form, DomConstants.FORM_TAG);
+
+		final FormElementsIterator iterator = new FormElementsIterator();
+		iterator.setForm(form);
+		return iterator;
+	}
+
+	/**
+	 * This iterator also implements Destroyable. This faciliates allowing the
+	 * user to cleanup once the iterator has been used/exhausted.
+	 */
+	static class FormElementsIterator implements Iterator, Destroyable {
+
+		public boolean hasNext() {
+			return this.getCursor() < DOM.getElementPropertyInt(this.getForm(), DomConstants.LENGTH_PROPERTY);
+		}
+
+		public Object next() {
+			final int cursor = this.getCursor();
+			final Object object = this.next0(this.getForm(), cursor);
+			this.setCursor(cursor + 1);
+			return object;
+		}
+
+		native private Element next0(final Element form, final int index)/*-{
+		 var element = form.elements[ index ];
+		 return element ? element : null;
+		 }-*/;
+
+		public void remove() {
+			throw new UnsupportedOperationException("Form elements may not be removed using this iterator. this: " + this);
+		}
+
+		public void destroy() {
+			this.clearForm();
+		}
+
+		Element form;
+
+		Element getForm() {
+			return form;
+		}
+
+		void setForm(final Element form) {
+			this.form = form;
+		}
+
+		void clearForm() {
+			this.form = null;
+		}
+
+		int cursor = 0;
+
+		int getCursor() {
+			return cursor;
+		}
+
+		void setCursor(final int cursor) {
+			this.cursor = cursor;
+		}
+	}
+
+	/**
+	 * Helper which attempts to find and fetch an element belonging to the given
+	 * form by name.
+	 * 
+	 * @param form
+	 * @param elementName
+	 * @return
+	 */
+	public static Element getFormElement(final Element form, final String elementName) {
+		Checker.notNull("parameter:form", form);
+		Checker.notEmpty("parameter:elementName", elementName);
+
+		return getFormElement0(form, elementName);
+	}
+
+	native private static Element getFormElement0(final Element form, final String elementName)/*-{
+	 var element = null;
+
+	 element = form.elements[ elementName ];
+	 return element ? element : null;}-*/;
+
 }
