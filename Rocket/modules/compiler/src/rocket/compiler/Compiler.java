d61f9827778950edb9528bb22e65cc4211f41f7a
miroslav.pokorny <miroslav.pokorny@c50fa27a-1f1b-0410-a12a-0d92cb1c21c8>
2008-03-09 08:48:56 +0000

    * Added helper methods to Compiler to get class + method name, and to get source of a given jsNode.

--- Rocket/modules/compiler/src/rocket/compiler/Compiler.java
+++ Rocket/modules/compiler/src/rocket/compiler/Compiler.java
@@ -15,7 +15,11 @@
  */
 package rocket.compiler;
 
+import com.google.gwt.dev.jjs.ast.JClassType;
+import com.google.gwt.dev.jjs.ast.JMethod;
 import com.google.gwt.dev.jjs.ast.JNode;
+import com.google.gwt.dev.jjs.ast.JReferenceType;
+import com.google.gwt.dev.js.ast.JsNode;
 
 /**
  * Convenient base class with some common functionality
@@ -28,6 +32,26 @@ public class Compiler {
 	 * @return
 	 */
 	static public String getSource(final JNode node) {
+		return node.toSource().replaceAll( "\n", "\\\\n" ).replaceAll( "\r", "\\\\r" );
+	}
+	
+	/**
+	 * Builds the fully qualified name of the method including the enclosing class.
+	 * @param method
+	 * @return
+	 */
+	static public String getFullName( final JMethod method ){
+		final JReferenceType enclosingType = method.getEnclosingType();
+		final String methodName = method.getName();
+		return enclosingType == null ? methodName : enclosingType.getName() + '.' + methodName;
+	}
+	
+	/**
+	 * Extracts the source equivalent of the given node replacing newlines and carriage returns with their escaped slash equivalent.
+	 * @param node
+	 * @return
+	 */
+	static public String getSource(final JsNode node) {
 		return node.toSource().replaceAll( "\n", "\\\\n" ).replaceAll( "\r", "\\\\r" );
 	}
 }
