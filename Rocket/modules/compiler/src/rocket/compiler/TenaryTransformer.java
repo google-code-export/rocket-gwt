a84215fc7583940aa8d9fd7b103779b58aa22edc
miroslav.pokorny <miroslav.pokorny@c50fa27a-1f1b-0410-a12a-0d92cb1c21c8>
2008-03-13 09:14:06 +0000

    * Corrected a bug where NPE were occassionally occuring within TenaryTransformer.

--- Rocket/modules/compiler/src/rocket/compiler/TenaryTransformer.java
+++ Rocket/modules/compiler/src/rocket/compiler/TenaryTransformer.java
@@ -38,14 +38,17 @@ import com.google.gwt.dev.jjs.ast.JStatement;
 import com.google.gwt.dev.jjs.ast.JVariableRef;
 
 /**
- * Common base class for any optimiser that attempts to convert if statements into a tenary or conditional statement
- * equivalent.
+ * Common base class for any optimiser that attempts to convert if statements
+ * into a tenary or conditional statement equivalent.
+ * 
  * @author Miroslav Pokorny
  */
 abstract class TenaryTransformer {
 
 	/**
-	 * Visits all if statements passing each one to be processed by the {@link #visitIfStatement(JIfStatement, Context, TreeLogger)}.
+	 * Visits all if statements passing each one to be processed by the
+	 * {@link #visitIfStatement(JIfStatement, Context, TreeLogger)}.
+	 * 
 	 * @param jprogram
 	 * @param logger
 	 * @return
@@ -60,17 +63,22 @@ abstract class TenaryTransformer {
 			}
 		};
 		visitor.accept(jprogram);
-		return visitor.didChange();
+
+		final boolean changed = visitor.didChange();
+		branch.log(TreeLogger.DEBUG, changed ? "One or more changes were made." : "No changes were committed.", null);
+		return changed;		
 	}
 
 	/**
-	 * Sub classes must override this method to do something with the given if statement.
+	 * Sub classes must override this method to do something with the given if
+	 * statement.
+	 * 
 	 * @param ifStatement
 	 * @param context
 	 * @param logger
 	 */
 	abstract protected void visitIfStatement( JIfStatement ifStatement, Context context, TreeLogger logger );
-	
+
 	/**
 	 * Helper which tests if the given if statement has an accompanying else.
 	 * 
@@ -92,9 +100,16 @@ abstract class TenaryTransformer {
 	 */
 	protected JBinaryOperation findAssignment(final JStatement statement, final TreeLogger logger) {
 		JBinaryOperation assignment = null;
-	
+
 		JNode node = statement;
 		while (true) {
+			if( null == node ){
+				if( logger.isLoggable( TreeLogger.DEBUG)){
+					logger.log( TreeLogger.DEBUG, "Empty statement - not modified.", null );
+				}
+				break;
+			}
+
 			if (node instanceof JExpressionStatement) {
 				final JExpressionStatement expressionStatement = (JExpressionStatement) node;
 				final JExpression expression = expressionStatement.getExpr();
@@ -102,7 +117,7 @@ abstract class TenaryTransformer {
 					logger.log( TreeLogger.DEBUG, "Content of statement is not an expression but a " + expression.getClass().getName() + " - not modified.", null );
 					break;
 				}
-	
+
 				// lhs has to be a JVariableReference, op JBinaryOperator "=",
 				// rhs ignore
 				final JBinaryOperation binaryOperation = (JBinaryOperation) expression;
@@ -111,50 +126,55 @@ abstract class TenaryTransformer {
 					logger.log( TreeLogger.DEBUG, "Left hand side of BinaryOperator is not a variable reference - not modified.", null );
 					break;
 				}
-	
+
 				final JBinaryOperator binaryOperator = binaryOperation.getOp();
 				if (false == this.isCompatibleAssignment( binaryOperator, logger )) {
 					break;
 				}
-	
+
 				// found assignment stop searching...
 				assignment = binaryOperation;
 				break;
 			}
-	
+
 			if (false == (node instanceof JBlock)) {
-				logger.log( TreeLogger.DEBUG, "Found a non assignment statement, type: " + node.getClass().getName() + " - not modified.", null );
+				if( logger.isLoggable( TreeLogger.DEBUG )){
+					logger.log( TreeLogger.DEBUG, "Found a non assignment statement, type: " + node.getClass().getName() + " - not modified.", null );
+				}
 				break;
 			}
-	
+
 			final JBlock block = (JBlock) node;
 			final int blockStatementCount = block.statements.size(); 
 			if ( blockStatementCount != 1 ) {
-				logger.log( TreeLogger.DEBUG, "Nested blocked contains " + blockStatementCount + " statements when 1 is expected - not modified.", null );
+				if( logger.isLoggable( TreeLogger.DEBUG )){
+					logger.log( TreeLogger.DEBUG, "Nested blocked contains " + blockStatementCount + " statements when 1 is expected - not modified.", null );
+				}
 				break;
 			}
-	
+
 			// try again within the statement inside the block.
 			node = (JStatement) block.statements.get(0);
 		}
-	
+
 		return assignment;
 	}
-	
+
 	abstract protected boolean isCompatibleAssignment( JBinaryOperator operator, TreeLogger logger ); 
 
 	/**
-	 * This method clones the given variable reference, returning a new instance of the same type.
+	 * This method clones the given variable reference, returning a new instance
+	 * of the same type.
 	 * 
 	 * @param source
 	 * @return
 	 */
 	protected JVariableRef clone(final JVariableRef source) {
 		JVariableRef copy = null;
-	
+
 		final JProgram program = source.getJProgram();
 		final SourceInfo sourceInfo = source.getSourceInfo();
-	
+
 		while (true) {
 			if (source instanceof JFieldRef) {
 				final JFieldRef sourceField = (JFieldRef) source;
@@ -162,28 +182,28 @@ abstract class TenaryTransformer {
 				final JField field = sourceField.getField();
 				final JReferenceType enclosingType = sourceField.getEnclosingType();
 				copy = new JFieldRef(program, sourceInfo, instance, field, enclosingType);
-	
+
 				break;
 			}
-	
+
 			if (source instanceof JLocalRef) {
 				final JLocalRef sourceLocal = (JLocalRef) source;
 				final JLocal local = sourceLocal.getLocal();
 				copy = new JLocalRef(program, sourceInfo, local);
 				break;
 			}
-	
+
 			if (source instanceof JParameterRef) {
 				final JParameterRef sourceParameter = (JParameterRef) source;
 				final JParameter parameter = sourceParameter.getParameter();
-	
+
 				copy = new JParameterRef(program, sourceInfo, parameter);
 				break;
 			}
-	
+
 			throw new AssertionError("Then statement belonging to an optimisable if contains an unknown JVariableRef type: "+ source.getClass());
 		}
-	
+
 		return copy;
 	}
 }
