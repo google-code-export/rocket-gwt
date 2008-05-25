69ab99a50bbdb9f958c2d19f5de27026a7cf590e
miroslav.pokorny <miroslav.pokorny@c50fa27a-1f1b-0410-a12a-0d92cb1c21c8>
2008-05-25 11:33:47 +0000

    Mistakenly tagged.

--- tags/0.50/Rocket/modules/serialization/src/rocket/serialization/benchmark/client/Apple.java
+++
@@ -1,9 +0,0 @@
-package rocket.serialization.benchmark.client;
-
-public class Apple implements Fruit {
-	String colour;
-	
-	public boolean equals( final Object otherObject ){
-		return otherObject instanceof Apple ? this.colour.equals( ((Apple)otherObject).colour) : false;
-	}
-}
--- tags/0.50/Rocket/modules/serialization/src/rocket/serialization/benchmark/client/Fly.java
+++
@@ -1,9 +0,0 @@
-package rocket.serialization.benchmark.client;
-
-public class Fly extends Pest{
-	int eyeCount;
-	
-	public boolean equals( final Object otherObject ){
-		return otherObject instanceof Fly ? ((Fly) otherObject ).eyeCount == this.eyeCount : false;
-	}
-}
